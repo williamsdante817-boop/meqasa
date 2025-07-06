bf032eaaf1c5d658151bcb5b5816f97b481c6dad
Clement <clementnyankuadjei@gmail.com>
2025-07-06 11:10:23 +0000

    feat: enhance CommonFilters and MoreFiltersPopover components
    - Added a new prop to CommonFilters to conditionally hide the property type filter, improving flexibility in filter display.
    - Updated the property type select options in CommonFilters to include a default "Property Type" option.
    - Introduced new select items for "Any Period" and "Any Sort" in MoreFiltersPopover, enhancing filter options for users.

--- src/components/search/CommonFilters.tsx
+++ src/components/search/CommonFilters.tsx
@@ -16,38 +16,54 @@ import { MoreFiltersPopover } from "./MoreFiltersPopover";
 
 interface CommonFiltersProps {
   showMoreFilters?: boolean;
+  hidePropertyType?: boolean;
   formState: FormState;
   updateFormState: (updates: Partial<FormState>) => void;
 }
 
 export function CommonFilters({
   showMoreFilters = false,
+  hidePropertyType = false,
   formState,
   updateFormState,
 }: CommonFiltersProps) {
   return (
     <div className="relative mx-auto mt-3 hidden max-w-max items-end justify-center gap-2 py-1 lg:flex">
-      <Select
-        value={formState.propertyType}
-        onValueChange={(value) => updateFormState({ propertyType: value })}
-      >
-        <SelectTrigger className="h-5 min-w-[150px] max-w-[150px] cursor-pointer rounded-none border-0 bg-transparent py-0 text-base font-medium shadow-none text-white focus:border-0 focus:ring-0 focus:ring-transparent">
-          <SelectValue placeholder="Type" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectItem value="- Any -" key="default-type" className="line-clamp-1">
-              All Types
-            </SelectItem>
-            {searchConfig.selectOptions.propertyType.map(({ value, label }) => (
-              <SelectItem value={value} key={value} className="line-clamp-1">
-                {label}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Separator orientation="vertical" className="h-[20px] bg-[#9A9EB5]" />
+      {!hidePropertyType && (
+        <>
+          <Select
+            value={formState.propertyType}
+            onValueChange={(value) => updateFormState({ propertyType: value })}
+          >
+            <SelectTrigger className="h-5 min-w-[150px] max-w-[150px] cursor-pointer rounded-none border-0 bg-transparent py-0 text-base font-medium shadow-none text-white focus:border-0 focus:ring-0 focus:ring-transparent">
+              <SelectValue placeholder="Type" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem
+                  value="all"
+                  key="default-type"
+                  className="line-clamp-1"
+                >
+                  Property Type
+                </SelectItem>
+                {searchConfig.selectOptions.propertyType.map(
+                  ({ value, label }) => (
+                    <SelectItem
+                      value={value}
+                      key={value}
+                      className="line-clamp-1"
+                    >
+                      {label}
+                    </SelectItem>
+                  ),
+                )}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Separator orientation="vertical" className="h-[20px] bg-[#9A9EB5]" />
+        </>
+      )}
       <Select
         value={formState.bedrooms}
         onValueChange={(value) => updateFormState({ bedrooms: value })}
@@ -57,7 +73,11 @@ export function CommonFilters({
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
-            <SelectItem value="- Any -" key="default-beds" className="line-clamp-1">
+            <SelectItem
+              value="- Any -"
+              key="default-beds"
+              className="line-clamp-1"
+            >
               Bedrooms
             </SelectItem>
             {searchConfig.selectOptions.bedrooms.map(({ value, label }) => (
